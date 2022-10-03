efd8c8f1a8366966e864ef83327f91fef302e1aa
Tal Weiss <major.tal@gmail.com>
2022-10-03 04:04:11 +0200

    I have the gallery!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageGallery from 'react-image-gallery';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}, {props.name2}</h1>;
+}
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 function App() {
   return (
@@ -7,7 +27,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and rave to reload.
+          Edit <code>src/App.js</code> and raven to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +37,11 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Hello, world</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        <Welcome name="Sara" name2="asd"/>
+        <Welcome name="Cahal" />
+        <ImageGallery items={images} />;
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// const element = <Welcome name="Sara" name2="asd"/>;
+// root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
